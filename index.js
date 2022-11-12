40af316ec59ecb1128aef0be5b829b35c7f45bfd
xsoltek <46493628+xsoltek@users.noreply.github.com>
2022-11-12 16:39:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ const fetchCookie = (domain = 'fr') => {
             const sessionCookie = res.headers.get('set-cookie');
             controller.abort();
             resolve(cookie.parse(sessionCookie)['secure, _vinted_fr_session']);
-            console.log(resolve(cookie.parse(sessionCookie)['secure, _vinted_fr_session']))
         }).catch(() => {
             controller.abort();
             reject();
@@ -94,8 +93,8 @@ const search = (url, disableOrder = false, allowSwap = false, customParams = {})
         }
 
         const cachedCookie = cookies.get(domain);
-        const cookie = cachedCookie && cachedCookie.createdAt > Date.now() - 60_000 ? cachedCookie.cookie : await fetchCookie(domain).catch(() => {});
-        console.log(cookie)
+       // const cookie = cachedCookie && cachedCookie.createdAt > Date.now() - 60_000 ? cachedCookie.cookie : await fetchCookie(domain).catch(() => {});
+        const cookie = "{_vinted_fr_session: "eCtJMjJxV2FnZVdZT0UwOXZYTVNLaERqeVVRMkFWTlM5YnRXQlNBbW95elVCL2Rzb2R5QlhwbG5WS0NITnhuVGd4WHcvaXBDK3BpODRyL1hsQVk4cVdGR3hDYTNxMTJUTzhHMEJ1NHV5UlQyekpwWlcveEx3cFhwblNUUUgwOEk5THM3dXVJc3p1Tmd4aERqMVMxZGxGYk5GZGFxYkVwdldTbVBsaWFlOTMwSFB1UUYrSlVhOElsS0lhTmZqZlpOd1ExWElOMXBvMXYrRWhad29uNXNEU2gzYytQdWVSTktIeW1lZjdIazdQS2Q0QisrTEdkaEVDcHVBdm9FZlNKUS9YV1VLVHgzN05UWDBXdnN2MWt3cW5rL29ETzFnQlg1dHluYUJVNkNFa2wzbSt6cmJzaDdnclpRM1prMkNYRmdVTUdjSFhKU29ZdlVSckFTdWpkWU9ZK2hqbFJvZk5uS0ExT0NqTVZOZThoMjJ6ZDVUamhqTDFYQWVoUHAwQ25hL3N5VFNHL1J1aGNxR0RpNDl5MWlITlZkVnZCNE43elkxMkFBd2JnbXJzU3pqeDI5M2pZdmlsVGVPTUZwMC9GWGtHNHMxNitEMnFvQzlLcmlzWndRc1UrNERrUTNLRXJLdVRsSnVzNnpybzErdmd5b2JKTlRIQ0tCeVgyQWo3bzY5NnlybURWUzVXOVVXVnliQm10RFRtTU5tOVloNVdzd0FPTUEvZVdhT2dmMmJoS0JJTmZDZjlSaVp2c29NYkpSdWprMzdOUVBBckJOQkpjRDFrWWQwc1FpdzgrY3J0eWNVSkZ0N0xTa0Q0R1o4Q1d2T3kydWtPdTcxdG5ydEMzUktZWHdNK3gvYUlJdStQazFhZnkrME01eWk3V3BuLzVPV2VraTAydWM0MW5XTnB3S01kOXY2eGdzZy9TZXUxcVc1Q2tvQjRTdVBkZ25qY2U1a1NHeEcxVHdEc3dPOWpMUlJiRTRRTWNzeFRKOXFSd2hCYXhTTWpmeTRJc1hqNnFlUlpWR0RtY2JwcEYwL3ozQlN2SHdnUjBlNUx6eHphRHZsMFoyMnUxNkVjZEVDTTQ0NStKaVh5eTU0SXBHMDV4ZUs4MS8wRVg0a0s0SkUzL2tiUmZtWSs1dHU3cjQ1WkxzMWpwWVR0dTNxV3NMRHA5ZFRWU0FLVWFLTHR5NDFxbHFnbTRkUlU2b1VxOUZMNkdRWW5OTzZDb0c4NENsQ1o5eFp5VllwZHBLKzIyaFE1VFI4Zm9ZT2plNlNoNEFOT0libC8rMjQ2ckQrSEQ3d3owQS9MVFpZdDE3clZPR2VhQkUzUnpKdlZoQVgyUEJacXYvMjdTQTF2Vm95ZEVqenZjWUpZVW8xRHBOTWhENDBhR1JRZlhHZnE1ZXB6NGpYNHRaR0lna09DSm9qOWJXYjhhc1ZMMFZCeTd6QndqZjRkWU1IL25kU3NyTE5za3QwTVVsQ0tzVUM4eWVoL3plbk9CbzJuWE5WQ29YT0pjS0V6RStLZHpwQXBXeTh1YVFsYm5Ia3Mxak1KaG5TWi9mT0ppUWw2OFhSMTViUk13dVNSZjFabE4vQWNpSGRyOD0tLXYzRG11ZnU2UHN5RXcyRkZDTWI0elE9PQ%3D%3D--b4afc813c1e8748d8b74de9423bc0cda83a053b7"}"
         if (!cookie) {
             return reject('Could not fetch cookie');
         }
